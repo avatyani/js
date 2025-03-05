db8105faa6e73fdf91f002ea7529679d167deee8
avatyani <164971024+avatyani@users.noreply.github.com>
2025-03-05 23:44:47 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat al JS code as newer version
+
+// alert(3+3) //gives error because we are using nodejs, not browser 
+
+console.log (3 + 
+    3
+) // Code readability should be high
+
+console.log("Amit") 
+
+let name = "Amit"
+let age = 23
+let isLoggedIn = true
+
+// Primitive Datatypes
+// number => 2 to power 53
+// bigint
+// string => " ", ' '
+// boolean => true/false
+// null => standalone value
+// undefined => variable declared but value not assigned
+// symbol => to find uniqueness
+
+// object
+
+console.log(typeof "Amit") // string
+console.log(typeof age) //number
+console.log(typeof null)   //object
+console.log(typeof undefined)  // undefined
