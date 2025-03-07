87c0eb96d762d01917734490223970310a5005ae
avatyani <164971024+avatyani@users.noreply.github.com>
2025-03-07 00:21:49 +0000

    memory types

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -44,3 +44,35 @@ console.log(typeof myFunction);     // object function
 
 // link to read
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+
+
+
+// ****************** Memory **************************
+
+// Stack (primitive) & Heap (Non-primitive)
+
+// Stack - takes a copy (boolean, number, string)
+
+let youtubeName = "avatyani"
+let newyoutubeName = youtubeName
+
+newyoutubeName = "amit vatyani"
+
+console.log(youtubeName);       // avatyani
+console.log(newyoutubeName);    // amit vatyani
+// value is not updated because newyoutubeName takes a copy of youtubeName
+
+// Heap - takes a reference (object)
+
+let userOne = {
+    email: "av@google.com",
+    age: 22
+}
+
+let userTwo = userOne
+userTwo.email = "userTwo@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// userOne email value is also updated because both the users take reference for the heap memory
